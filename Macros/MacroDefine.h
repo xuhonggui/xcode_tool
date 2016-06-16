78ead4038ed71065c00372be024e177a885952ab
xuhonggui <790786581@qq.com>
2016-06-16 07:32:39 +0000

    常用宏定义

---
+++ Macros/MacroDefine.h
@@ -0,0 +1,93 @@
+//
+//  MacroDefine.h
+//  eFreshMall
+//
+//  Created by zlp on 16/4/28.
+//  Copyright © 2016年 cnmobi. All rights reserved.
+//
+
+#ifndef MacroDefine_h
+#define MacroDefine_h
+
+/// 常用宏定义
+#define kUserDefaults       [NSUserDefaults standardUserDefaults]
+
+#define kNotificationCenter [NSNotificationCenter defaultCenter]
+
+#define kApplication        [UIApplication sharedApplication]
+
+#define kAppDelegate        (AppDelegate *)[[UIApplication sharedApplication] delegate]
+
+#define kFileManager        [NSFileManager defaultManager]
+
+#define kBundle             [NSBundle mainBundle]
+
+#define kDevice             [UIDevice currentDevice]
+
+#define kWindow             [[UIApplication sharedApplication] keyWindow]
+
+#define kInfoDictionary     [[NSBundle mainBundle] infoDictionary]
+
+#define kBundleVersion      [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
+
+#define kDocumentPath       [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]
+
+#define kCachesDirectoryPath [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]
+
+#define kBundleLoadNibName(name) [[[NSBundle mainBundle] loadNibNamed:name owner:nil options:nil] lastObject]
+
+#define kDispatch_Global(block) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
+
+#define KDispatch_Main(block) dispatch_async(dispatch_get_main_queue(),block)
+
+#define kFMUser [FMUserInfo sharedInstance].user
+
+#define kNetWorkingTool [FMNetWorkingTool sharedInstance]
+
+#define kNetworkReachability [AFNetworkReachabilityManager sharedManager]
+
+#define WEAK_SELF()  __weak __typeof(self) weakSelf = self;
+
+#define kYUmengBeginPage(page) [MobClick beginLogPageView:page]
+
+#define kYUmengEndPage(page) [MobClick endLogPageView:page]
+
+//UIImage
+#define FMImage(name) [UIImage imageNamed:name]
+
+//屏幕的宽、高
+#define SCREEN_W ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_H ([UIScreen mainScreen].bounds.size.height)
+#define NAVIGATION_H 44
+#define TABBAR_H 49
+#define STATUSBAR_H 20
+#define MARGIN 10
+
+//判断iPhone型号
+#define isiPhone4  (SCREEN_H == 480)
+#define isiPhone5  (SCREEN_H == 568)
+#define isiPhone6  (SCREEN_H == 667)
+#define isiPhone6P (SCREEN_H == 736)
+
+
+//判断系统版本
+#define IOS_VERSION_6_OR_ABOVE ([[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0)
+#define IOS_VERSION_7_OR_ABOVE ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
+#define IOS_VERSION_8_OR_ABOVE ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
+/* 
+ 打印
+ __func__ 打印方法名 c字符串
+ __LINE__ 打印行数
+ ## "##"放在","和参数之间，那么如果参数留空的话，那么"##"前面的","就会删掉，从而防止编译错误。
+ ((void)0) 在C、C++中表示无任何操作（详见http://stackoverflow.com/questions/2198950/why-is-void-0-a-no-operation-in-c-and-c）
+ */
+#ifdef DEBUG
+//#define FMLog(...) NSLog(__VA_ARGS__)/* 不打印方法名、行数 */
+#define FMLog(format, ...) NSLog((@"%s(%d)"format), __func__, __LINE__, ##__VA_ARGS__)
+#else
+#define FMLog(...) ((void)0)
+#endif
+
+
+#endif /* MacroDefine_h */
---
+++ Macros/MacroFont.h
@@ -0,0 +1,66 @@
+//
+//  FM-Font.h
+//  eFreshMall
+//
+//  Created by zlp on 16/5/16.
+//  Copyright © 2016年 cnmobi. All rights reserved.
+//
+
+#ifndef FM_Font_h
+#define FM_Font_h
+
+/* 导航条 */
+#define kNavTitleFont (SCREEN_W <= 375 ? 18 : 20)
+
+#define kFont_22 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:22] : [UIFont systemFontOfSize:24])
+
+#define kFont_21 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:21] : [UIFont systemFontOfSize:23])
+
+#define kFont_20 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:20] : [UIFont systemFontOfSize:22])
+
+#define kFont_19 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:19] : [UIFont systemFontOfSize:21])
+
+#define kFont_18 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:18] : [UIFont systemFontOfSize:20])
+
+#define kFont_17 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:17] : [UIFont systemFontOfSize:19])
+
+#define kFont_16 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:16] : [UIFont systemFontOfSize:18])
+
+#define kFont_15 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:15] : [UIFont systemFontOfSize:17])
+
+#define kFont_14 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:14] : [UIFont systemFontOfSize:16])
+
+#define kFont_13 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:13] : [UIFont systemFontOfSize:15])
+
+#define kFont_12 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:12] : [UIFont systemFontOfSize:14])
+
+#define kFont_11 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:11] : [UIFont systemFontOfSize:13])
+
+#define kFont_10 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:10] : [UIFont systemFontOfSize:12])
+
+#define kFont_9 (SCREEN_W <= 375 ? [UIFont systemFontOfSize:9] : [UIFont systemFontOfSize:11])
+
+//字体大小
+#define FONT_27 [UIFont systemFontOfSize:27.0]
+#define FONT_26 [UIFont systemFontOfSize:26.0]
+#define FONT_25 [UIFont systemFontOfSize:25.0]
+#define FONT_24 [UIFont systemFontOfSize:24.0]
+#define FONT_23 [UIFont systemFontOfSize:23.0]
+#define FONT_22 [UIFont systemFontOfSize:22.0]
+#define FONT_21 [UIFont systemFontOfSize:21.0]
+#define FONT_20 [UIFont systemFontOfSize:20.0]
+#define FONT_19 [UIFont systemFontOfSize:19.0]
+#define FONT_18 [UIFont systemFontOfSize:18.0]
+#define FONT_17 [UIFont systemFontOfSize:17.0]
+#define FONT_16 [UIFont systemFontOfSize:16.0]
+#define FONT_15 [UIFont systemFontOfSize:15.0]
+#define FONT_14 [UIFont systemFontOfSize:14.0]
+#define FONT_13 [UIFont systemFontOfSize:13.0]
+#define FONT_12 [UIFont systemFontOfSize:12.0]
+#define FONT_11 [UIFont systemFontOfSize:11.0]
+#define FONT_10 [UIFont systemFontOfSize:10.0]
+#define FONT_9 [UIFont systemFontOfSize:9.0]
+#define FONT_8 [UIFont systemFontOfSize:8.0]
+#define FONT_7 [UIFont systemFontOfSize:7.0]
+
+#endif /* FM_Font_h */
